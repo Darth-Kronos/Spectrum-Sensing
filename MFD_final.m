4e2e0c7ae25684b9069995955debd35789713f7c
Yashaskara <43381463+Yashaskara@users.noreply.github.com>
2020-05-16 14:26:35 +0530

    Add files via upload

--- MFD_final.m
+++ MFD_final.m
@@ -1,7 +1,7 @@
 clc
 close all
 clear all
-samples = 0:8191;
+samples = 0:1/8191:1;
 fs = 4000;
 L = 8192;
 ts = 1/fs;
@@ -9,19 +9,27 @@ fm = 16;
 f0 = 512;
 l = 32;
 m = 256;
-ys = 2.*cos(2*pi*fm.*samples) .*cos(2*pi*f0.*samples);
-snr_dB = -25:10:45; % SNR in decibels
+zn = 2.*cos(2*pi*fm.*samples) .*cos(2*pi*f0.*samples);
+snr_dB = -22;% SNR in decibels
 snr = 10.^(snr_dB./10); % Linear Value of SNR
-pf = 0.01:0.01:1; % Pf = Probability of False Alarm
-%% Simulation to plot Probability of Detection (Pd) vs. Probability of False Alarm (Pf) 
-for a = 1:length(snr)
-for k = 1:length(pf)
-    Pd(k) = qfunc(qfuncinv(pf(k))-sqrt(snr(a)))
-    
+
+x=1;%Uncertainty in noise variance(I chose it randomly)
+beta = 10^(x/10);
+
+%% Simulation to plot Probability of Detection (Pd) vs. Probability of False Alarm (Pf)
+noise = sqrt(1/snr)*randn(1,L) +1i*sqrt(1/snr)*randn(1,L);  
+signal = sqrt(1/snr)*randn(1,L)+ 1i*sqrt(1/snr)*randn(1,L) + zn;
+taumfd_h0 = sum(noise.*zn);    
+taumfd_h1 = sum(signal.*zn);
+thresh = linspace((1/beta),beta,1000);
+for i = 1:length(thresh)
+    pf = find(taumfd_h0 >= thresh(i));
+    pd = find(taumfd_h1 >= thresh(i));
 end
-plot(pf, Pd)
-hold on
-end
-legend("-25db","-15db","5db","15db","25","35","45")
+
+
+%% Theoritical plot
+
+
 
 
