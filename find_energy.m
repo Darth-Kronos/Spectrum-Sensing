5e00807523eef5b28a330d4a2e8495e320849b0a
Purushothaman <purushothaman.syadav@gmail.com>
2020-08-03 21:45:12 +0530

    MFD Update

--- find_energy.m
+++ find_energy.m
@@ -1,7 +1,7 @@
 function energy = find_energy(signal,m,l,snr)
     y = reshape(signal,m,l);
-    Y = fft(y); % M point fft
+    Y = fft(y,m); % M point fft
     abs_Y = abs(Y).^2; 
     r = mean(abs_Y,1);
-    energy = (2*snr).*mean(r);
+    energy = (2*snr).*sum(r);
 end
