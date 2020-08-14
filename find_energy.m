4d1af6f04c85349cbd3452e6a084e22c909fb25b
Purushothaman <purushothaman.syadav@gmail.com>
2020-08-14 18:12:32 +0530

    Changed the cut off the frequency of low pass

--- find_energy.m
+++ find_energy.m
@@ -1,9 +1,9 @@
 function energy = find_energy(signal,fc,fs,samples,m,l,snr)
-    demodulated = signal.*cos(2*pi*fc/fs.*samples);
-    bandpass = myLowpass(demodulated,20,fs);
+    demodulated = 2*signal.*cos(2*pi*fc/fs.*samples);
+    bandpass = myLowpass(demodulated,120,fs);
     y = reshape(bandpass,m,l);
     Y = fft(y,m); % M point fft
     abs_Y = abs(Y).^2; 
-    r = mean(abs_Y,2);
+    r = mean(abs_Y,1);
     energy = (2*snr)*sum(r);
 end
