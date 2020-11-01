d052acb6885e74675b9161a86b07dfd65f329bb2
Purushothaman <purushothaman.syadav@gmail.com>
2020-11-01 22:46:34 +0530

    ROC for NB 1.0

--- Naive Bayes/NB.m
+++ Naive Bayes/NB.m
@@ -1,57 +1,20 @@
-clc
-close all
 clear all
-samples = 0:8191;
-fs = 4000;
-L = 8192;
-ts = 1/fs;
-fm = 16;
-f0 = 512;
-l = 32;
-m = 256;
-
-ys = 2.*cos(2*pi*fm/fs.*samples) .*cos(2*pi*f0/fs.*samples);
-snr_dB = -22; %SNR in decibels
-snr = 10.^(snr_dB./10); % Linear Value of SNR
-
-T = 2500;
-obs_H0 = zeros(1,T);
-obs_H1 = zeros(1,T);
-
-%% Training samples
-
-for i=1:T
-    noise = sqrt(1/snr)*randn(1,L);
-    Signal = sqrt(1/snr)*randn(1,L) + ys;
-    obs_H0(i) = find_energy(noise,f0,fs,samples,m,l,snr);
-    obs_H1(i) = find_energy(Signal,f0,fs,samples,m,l,snr);
-
+load('./SVM/data_set_10.mat');
+train_X = data_set(:,[1,2,3]);
+train_Y = data_set(:,4);
+load('test.mat');
+test_X = data_set(:,[1,2,3]);
+test_Y = data_set(:,4);
+p = 0:0.0001:1;
+pf = zeros(1,length(p));
+pd = zeros(1,length(p));
+for i=1:length(p)
+    prior = [p(i) 1-p(i)];
+    model = fitcnb(train_X,train_Y,'Prior',prior);
+    
+    y = predict(model,test_X);
+    pf(i) = sum(logical(y) & ~logical(test_Y))/(200-sum(test_Y));
+    pd(i) = dot(y,test_Y)/sum(test_Y);
 end
-train_X = cat(2,obs_H0,obs_H1);
-train_Y = cat(2, zeros(1,2500),ones(1,2500));
-Mdl = fitcnb(train_X',train_Y');
 
-%% Monte carlo
-
-M = 100;
-obs_H0 = zeros(1,M);
-obs_H1 = zeros(1,M);
-pf = zeros(1,M);
-pd = zeros(1,M);
-for i=1:M
-    for j=1:M
-        noise = sqrt(1/snr)*randn(1,L);
-        Signal = sqrt(1/snr)*randn(1,L) + ys;
-        obs_H0 = find_energy(noise,f0,fs,samples,m,l,snr);
-        obs_H1 = find_energy(Signal,f0,fs,samples,m,l,snr);
-        if(Mdl.predict(obs_H0) == 1)
-            pf(i) = pf(i) +1;
-        end
-        if(Mdl.predict(obs_H1) == 1)
-            pd(i) = pd(i) +1;
-        end
-    end
-end
-pf = pf./2500;
-pd = pd./2500;
-plot(pd,pf)
+plot(pf,pd)
