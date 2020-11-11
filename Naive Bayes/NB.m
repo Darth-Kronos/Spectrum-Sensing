89a92283dcdea2a8fb20e1684b618ccb86630a27
Purushothaman <purushothaman.syadav@gmail.com>
2020-11-11 18:56:17 +0530

    NB: Pe vs Prior Probability

--- Naive Bayes/NB.m
+++ Naive Bayes/NB.m
@@ -1,20 +1,23 @@
 clear all
 load('./SVM/data_set_10.mat');
-train_X = data_set(:,[1,2,3]);
+train_X = data_set(:,1);
 train_Y = data_set(:,4);
-load('test.mat');
-test_X = data_set(:,[1,2,3]);
+% load('test.mat');
+test_X = data_set(:,1);
 test_Y = data_set(:,4);
-p = 0:0.0001:1;
+p = 0:0.001:1;
 pf = zeros(1,length(p));
 pd = zeros(1,length(p));
+pe = zeros(1,length(p));
 for i=1:length(p)
     prior = [p(i) 1-p(i)];
     model = fitcnb(train_X,train_Y,'Prior',prior);
     
     y = predict(model,test_X);
-    pf(i) = sum(logical(y) & ~logical(test_Y))/(200-sum(test_Y));
+    pf(i) = sum(logical(y) & ~logical(test_Y))/(length(test_Y)-sum(test_Y));
     pd(i) = dot(y,test_Y)/sum(test_Y);
+    pe(i) = p(i)*pf(i) + (1-p(i))*(1-pd(i));
 end
 
-plot(pf,pd)
+plot(p,pe)
+xlabel('Prior Probability');ylabel('Probability of Error');
