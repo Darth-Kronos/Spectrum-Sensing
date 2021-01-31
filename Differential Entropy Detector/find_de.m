1781e5f2e283ad5bf25059f7d0316f409050649a
Purushothaman <purushothaman.syadav@gmail.com>
2021-01-31 10:55:52 +0530

    Differential Entropy v1

---
+++ Differential Entropy Detector/find_de.m
@@ -0,0 +1,6 @@
+function energy = find_de(signal,snr)
+%     demodulated = 2*signal.*cos(2*pi*fc/fs.*samples);
+%     bandpass = myLowpass(demodulated,120,fs);
+%     y = reshape(signal,m,l);
+    
+end
