6f617d2fd9f5ae91126fd20b2965d7ec84047410
Purushothaman <purushothaman.syadav@gmail.com>
2021-01-31 11:30:57 +0530

    Checking if differential entropy detector  = ED for gaussian noise

--- Differential Entropy Detector/find_de.m
+++ Differential Entropy Detector/find_de.m
@@ -1,6 +1,7 @@
-function energy = find_de(signal,snr)
+function de = find_de(signal,beta)
 %     demodulated = 2*signal.*cos(2*pi*fc/fs.*samples);
 %     bandpass = myLowpass(demodulated,120,fs);
 %     y = reshape(signal,m,l);
-    
+    alpha = log(beta*(sum(abs(signal-mean(signal)).^beta))/length(signal))/beta;
+    de = 1/beta - log(beta/(2*gamma(1/beta))) + alpha;
 end
