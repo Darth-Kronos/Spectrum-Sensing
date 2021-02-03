e1db6492c5c445b786c20a665fb616ecd5ce26d6
Purushothaman <purushothaman.syadav@gmail.com>
2021-02-03 21:30:05 +0530

    KNN DE

--- Dataset/main.m
+++ Dataset/main.m
@@ -1,12 +1,13 @@
 clear all;
 load('./Dataset/dataset_1.mat');
 raw_data = Pd_rawData(:,1);
-snr_db = -30:1:-10;
+snr_db = -30:-10;
 pd_svm = ones(1,length(snr_db));
 pd_lr = ones(1,length(snr_db));
 pd_knn = ones(1,length(snr_db));
 pd_svm_de = ones(1,length(snr_db));
 pd_lr_de = ones(1,length(snr_db));
+pd_knn_de = ones(1,length(snr_db));
 beta = 1.5;
 for i=1:length(snr_db)
     fprintf('Sl no. SNR: %d\n',i);
@@ -15,41 +16,44 @@ for i=1:length(snr_db)
     X = data_set(:,1);
     Y = data_set(:,2);
     fprintf('trianing \n');
+    model_knn  = fitcknn(X,Y,'NumNeighbors',35);
+    [~,score] = resubPredict(model_knn);
+    [pf_knn,pdd_knn_de,Tsvm,AUCsvm] = perfcurve(logical(Y),score(:,logical(model_knn.ClassNames)),'true');
     %SVM
-    mdlSVM = fitcsvm(X,Y,'Standardize',true);
-    mdlSVM = fitPosterior(mdlSVM);
-    [~,score_svm] = resubPredict(mdlSVM);
-    [pf_svm,pdd_svm,Tsvm,AUCsvm] = perfcurve(logical(Y),score_svm(:,logical(mdlSVM.ClassNames)),'true');
+%     mdlSVM = fitcsvm(X,Y,'Standardize',true);
+%     mdlSVM = fitPosterior(mdlSVM);
+%     [~,score_svm] = resubPredict(mdlSVM);
+%     [pf_svm_de,pdd_svm_de,Tsvm,AUCsvm] = perfcurve(logical(Y),score_svm(:,logical(mdlSVM.ClassNames)),'true');
     %Logistic
 %     model_lr = fitglm(X,Y,'Distribution','binomial','Link','logit');
 %     score_log = model_lr.Fitted.Probability;
 %     [pf_lr,pdd_lr,Tlog,AUClog] = perfcurve(data_set(:,2),score_log,'1');
     
-    pd_svm_de(i) = pdd_svm(find(pf_svm<=0.1, 1, 'last' ));
+%     pd_svm_de(i) = pdd_svm(find(pf_svm<=0.1, 1, 'last' ));
 %     pd_lr_de(i) = pdd_lr(find(pf_lr<=0.1, 1, 'last' ));
-    
+      pd_knn_de(i) = pdd_knn_de(find(pf_knn<=0.1, 1, 'last' ));
     %% ED
     %SVM
     data_set = preprocessing(raw_data,snr_db(i),beta);
     fprintf('ED\n');
     X = data_set(:,1);
     Y = data_set(:,2);
-    mdlSVM = fitcsvm(X,Y,'Standardize',true);
-    mdlSVM = fitPosterior(mdlSVM);
-    [~,score_svm] = resubPredict(mdlSVM);
-    [pf_svm,pdd_svm,Tsvm,AUCsvm] = perfcurve(logical(Y),score_svm(:,logical(mdlSVM.ClassNames)),'true');
+%     mdlSVM = fitcsvm(X,Y,'Standardize',true);
+%     mdlSVM = fitPosterior(mdlSVM);
+%     [~,score_svm] = resubPredict(mdlSVM);
+%     [pf_svm,pdd_svm,Tsvm,AUCsvm] = perfcurve(logical(Y),score_svm(:,logical(mdlSVM.ClassNames)),'true');
     %Logistic
 %     model_lr = fitglm(X,Y,'Distribution','binomial','Link','logit');
 %     score_log = model_lr.Fitted.Probability;
 %     [pf_lr,pdd_lr,Tlog,AUClog] = perfcurve(logical(Y),score_log,'1');
     %KNN
-%     model_knn  = fitcknn(X,Y,'NumNeighbors',35);
-%     [~,score] = resubPredict(model_knn);
-%     [pf_knn,pdd_knn,Tsvm,AUCsvm] = perfcurve(logical(Y),score(:,logical(model_knn.ClassNames)),'true');
+    model_knn  = fitcknn(X,Y,'NumNeighbors',35);
+    [~,score] = resubPredict(model_knn);
+    [pf_knn,pdd_knn,Tsvm,AUCsvm] = perfcurve(logical(Y),score(:,logical(model_knn.ClassNames)),'true');
     
-    pd_svm(i) = pdd_svm(find(pf_svm<=0.1, 1, 'last' ));
+%     pd_svm(i) = pdd_svm(find(pf_svm<=0.1, 1, 'last' ));
 %     pd_lr(i) = pdd_lr(find(pf_lr<=0.1, 1, 'last' ));
-%     pd_knn(i) = pdd_knn(find(pf_knn<=0.1, 1, 'last' ));
+    pd_knn(i) = pdd_knn(find(pf_knn<=0.1, 1, 'last' ));
 end
 
-save('all.mat')
+save('knn_1_5.mat')
